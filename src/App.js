a4fd9e48572e1744b06ca07d6a733de7c09241ff
sh2624 <sh2624@daum.net>
2026-01-27 15:46:48 +0900

    Fix: 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
